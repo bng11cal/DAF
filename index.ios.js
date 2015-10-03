99150c31abba5ad0b028fd55a95d6942baa1793b
Nicholas Cai <nicholascai@Nicholass-MacBook-Pro-3.local>
2015-10-03 01:57:36 -0700

    Testing

--- index.ios.js
+++ index.ios.js
@@ -17,7 +17,7 @@ var DAF = React.createClass({
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to React Native!
+          Welcome to ReacNative!
         </Text>
         <Text style={styles.instructions}>
           To get started, edit index.ios.js
